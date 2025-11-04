6d1e86d1073593874451c09ebf9884ad17d28de5
Phillip DuLion <phillip@dulion.com>
2025-11-03 16:04:05 -0800

    Update mainual-linux.sh TODOs

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -21,7 +21,10 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+if ! mkdir -p ${OUTDIR}; then
+    echo "Error: Unable to create output directory ${OUTDIR}"
+    exit 1
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -34,7 +37,12 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # PD: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -47,7 +55,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# PD: Create necessary base directories
+mkdir -p "$OUTDIR/rootfs"
+cd "$OUTDIR/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,12 +68,16 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # PD:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# PD: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
@@ -68,13 +85,27 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 
-# TODO: Make device nodes
+# PD: Make device nodes
+# mknod -m 666 dev/null c 1 3
+# mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# PD: Clean and build the writer utility
+# cd "$FINDER_APP_DIR"
+# make clean
+# make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# PD: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+# cp -a "$FINDER_APP_DIR"/writer "$OUTDIR"/rootfs/home
+# cp -a "$FINDER_APP_DIR"/finder.sh "$OUTDIR"/rootfs/home
+# cp -a "$FINDER_APP_DIR"/finder-test.sh "$OUTDIR"/rootfs/home
+# cp -a "$FINDER_APP_DIR"/conf "$OUTDIR"/rootfs/home
+# cp -a "$FINDER_APP_DIR"/autorun-qemu.sh "$OUTDIR"/rootfs/home
 
-# TODO: Chown the root directory
+# PD: Chown the root directory
+# cd "$OUTDIR/rootfs"
+# sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+# PD: Create initramfs.cpio.gz
+# cd "$OUTDIR/rootfs"
+# find . | cpio -H newc -ov --owner root:root | gzip -9 > ${OUTDIR}/initramfs.cpio.gz
